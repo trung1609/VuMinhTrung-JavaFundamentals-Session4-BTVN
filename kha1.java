768d554b9d14bb0666408093f8827eb6c52f7b72
Vũ Minh Trung <167429293+trung1609@users.noreply.github.com>
2025-12-09 11:04:37 +0700

    Add files via upload

---
+++ kha1.java
@@ -0,0 +1,35 @@
+package Session4;
+
+import java.util.Scanner;
+
+public class kha1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap kich thuoc cua mang: ");
+        int n = Integer.parseInt(sc.nextLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Nhap gia tri cho phan tu thu %d: ", i);
+            arr[i] = Integer.parseInt(sc.nextLine());
+        }
+        System.out.print("Mang truoc khi sap xep: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+
+        //Sap xep
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.print("\nMang sau khi sap xep: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+    }
+}
